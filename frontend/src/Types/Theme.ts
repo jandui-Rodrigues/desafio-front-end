3cf6e85fb29e1b030a0fb39c8b6389bdadc1a79b
jandui <janduineto18@gmail.com>
2024-04-07 12:36:39 -0300

    Feat(styled-components): Add styled-components, globalstyled components, and a theme to the application

---
+++ frontend/src/Types/Theme.ts
@@ -0,0 +1,23 @@
+type Theme = {
+  colors: {
+    primary: string;
+    secondary: string;
+    black: string;
+    white: string;
+    "gray-00": string;
+    "gray-10": string;
+    "gray-20": string;
+  };
+  spacing: {
+    xs: string;
+    sm: string;
+    md: string;
+    lg: string;
+    xl: string;
+    xxl: string;
+    xxxl: string;
+    xxxxl: string;
+  }
+};
+
+export default Theme;
---
+++ frontend/src/style/theme.tsx
@@ -0,0 +1,25 @@
+import Theme from "../Types/Theme";
+
+const theme: Theme = {
+  colors: {
+    primary: "#5A84C0",
+    secondary: "#594ED2",
+    black: "#1c1c1c",
+    white: "#FFFFFF",
+    "gray-00": "#F0F0F0",
+    "gray-10": "#DFDFDF",
+    "gray-20": "#9E9E9E",
+  },
+  spacing: {
+    xs: "4px",
+    sm: "8px",
+    md: "16px",
+    lg: "20px",
+    xl: "32px",
+    xxl: "40px",
+    xxxl: "60px",
+    xxxxl: "80px",
+  },
+};
+
+export default theme;
