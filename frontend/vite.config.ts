668a4a9c3e2cdc8b582530094d9d438a73d9e7f4
jandui <janduineto18@gmail.com>
2024-04-07 12:33:13 -0300

    Chore: Update Vite config and add Style-components dependencies and their types

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const port = process.env.PORT || 3000
+const PORT = Number(port)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: PORT,
+  },
 })
